c46fa67dbe972063e838c5c11900441421b2ad7e
Michal Nowak <alejajestemmondry@gmail.com.pl>
2015-04-25 13:04:30 +0200

    Creating graph

--- crawler.py
+++ crawler.py
@@ -12,16 +12,35 @@ def search(query):
 	data = json.loads(response.read())
 	return data
 
-def add_to_neo4j(data):
+def get_people_from_db()
+	# TODO
+	return []
+
+def add_to_db(data):
 	graph = Graph()
 	tx = graph.cypher.begin()
-	for d in data:
-		tx.append("TODO QUERY")
+	for d in data['children']:
+		tx.append("TODO QUERY") # TODO
 
 if __name__ == '__main__':
-	if len(sys.argv) < 2:
-		print 'Usage: ' + sys.argv[0] + ' <query>'
-	else:
-		data = search(sys.argv[1])
-		print data
+	g = []
+	for person in get_people_from_db():
+		peron_name = person['name'] # TODO
+		data = search(person_name)
+		for cmt in data:
+			existing_nodes = [n for n in g if n['person'] == person_name]
+
+			if len(existing_nodes) > 0:
+				for e_node in existing_nodes:
+					mentioned_person = {]
+					mentioned_person['id'] = data['data']['id']
+					metioned_person['person'] = person_name
+					e_node['mentioned'].add(mentioned_person)
+					g.add(e_node)
+			
+			node = {}
+			node['id'] = data['data']['id']
+			node['person'] = person_name
+			node['mentioned'] = []
+			g.add(node)
 
