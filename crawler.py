3150dc727fd809a12f5ca38b0de84066b20273f9
Michal Nowak <alejajestemmondry@gmail.com.pl>
2015-05-10 21:04:17 +0200

    Getting all people nodes from db

--- crawler.py
+++ crawler.py
@@ -1,10 +1,11 @@
 import urllib
 import json
 import sys
-from py2neo import Graph, Path
+from py2neo import Graph, Path, cypher
 
 reddit_page = 'http://www.reddit.com/'
 subreddit = 'r/history'
+graph = Graph()
 
 def search(query):
 	url = reddit_page + subreddit + '/search.json?q=' + query + '&restrict_sr=1'
@@ -12,35 +13,38 @@ def search(query):
 	data = json.loads(response.read())
 	return data
 
-def get_people_from_db()
-	# TODO
-	return []
+def handle_node(node):
+	print node[0]
+
+def get_people_from_db():
+	results = graph.cypher.execute("MATCH (a)-[:`http://xmlns.com/foaf/0.1/name`]->(b) RETURN b")
+	return [ r.b.properties['value'] for r in results]
 
 def add_to_db(data):
-	graph = Graph()
 	tx = graph.cypher.begin()
 	for d in data['children']:
 		tx.append("TODO QUERY") # TODO
 
 if __name__ == '__main__':
 	g = []
-	for person in get_people_from_db():
-		peron_name = person['name'] # TODO
-		data = search(person_name)
-		for cmt in data:
-			existing_nodes = [n for n in g if n['person'] == person_name]
-
-			if len(existing_nodes) > 0:
-				for e_node in existing_nodes:
-					mentioned_person = {]
-					mentioned_person['id'] = data['data']['id']
-					metioned_person['person'] = person_name
-					e_node['mentioned'].add(mentioned_person)
-					g.add(e_node)
-			
-			node = {}
-			node['id'] = data['data']['id']
-			node['person'] = person_name
-			node['mentioned'] = []
-			g.add(node)
+	print get_people_from_db()
+#	for person in get_people_from_db():
+#		person_name = person['name'] # TODO
+#		data = search(person_name)
+#		for cmt in data:
+#			existing_nodes = [n for n in g if n['person'] == person_name]
+#
+#			if len(existing_nodes) > 0:
+#				for e_node in existing_nodes:
+#					mentioned_person = {]
+#					mentioned_person['id'] = data['data']['id']
+#					metioned_person['person'] = person_name
+#					e_node['mentioned'].add(mentioned_person)
+#					g.add(e_node)
+#			
+#			node = {}
+#			node['id'] = data['data']['id']
+#			node['person'] = person_name
+#			node['mentioned'] = []
+#			g.add(node)
 
